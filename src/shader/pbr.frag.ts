0b945e656b8b5947ce13e56d79eec56c2023e458
Natalia Nikoladze <natalia.nikoladze@epita.fr>
2025-09-26 15:58:58 +0200

    fix: specular

--- src/shader/pbr.frag.ts
+++ src/shader/pbr.frag.ts
@@ -28,7 +28,7 @@ uniform Light uLights[10]; // 10 = max number of lights
 uniform int NB_LIGHTS;
 
 float PI = 3.141592654;
-vec3 roughness = vec3(0.5);
+float roughness = 0.1;
 
 // From three.js
 vec4 sRGBToLinear( in vec4 value ) {
@@ -40,44 +40,40 @@ vec4 LinearTosRGB( in vec4 value ) {
 	return vec4( mix( pow( value.rgb, vec3( 0.41666 ) ) * 1.055 - vec3( 0.055 ), value.rgb * 12.92, vec3( lessThanEqual( value.rgb, vec3( 0.0031308 ) ) ) ), value.a );
 }
 
-vec3 FresnelSchlick(vec3 f0, vec3 w_i, vec3 w_o) {
-  return f0 + (1.0 - f0) * pow(clamp(1.0 - dot(w_o, w_i), 0.0, 1.0), 5.0);
+float FresnelSchlick(float f0, vec3 w_i, vec3 w_o) {
+  return f0 + (1.0 - f0) * 1.0 - pow(1.0 - clamp(dot(w_o, w_i), 0.0, 1.0), 5.0);
 }
 
-float norm(vec3 v) {
-  return sqrt(pow(v.x + v.y + v.z, 2.0));
-}
-
-vec3 normal_distrib(vec3 w_o, vec3 w_i) {
-  vec3 h = (w_i + w_o) / norm(w_i + w_o);
-  vec3 num = pow(roughness, vec3(2.0));
-  vec3 denom = PI * pow(pow(dot(vNormalWS, h), 2.0) * (num - 1.0) + 1.0, vec3(2.0));
-  vec3 D = num / denom;
+float normal_distrib(vec3 w_o, vec3 w_i) {
+  vec3 h = normalize((w_i + w_o) / length(w_i + w_o));
+  float num = pow(roughness, 2.0);
+  float denom = PI * pow(pow(clamp(dot(vNormalWS, h), 0.0, 1.0), 2.0) * (num - 1.0) + 1.0, 2.0);
+  float D = num / denom;
   return D;
 }
 
-vec3 GSchlick(vec3 w, vec3 k) {
-  float num = dot(vNormalWS, w);
-  vec3 denom = num * (1.0 - k) + k;
+float GSchlick(vec3 w, float k) {
+  float num = clamp(dot(vNormalWS, w), 0.0, 1.0);
+  float denom = num * (1.0 - k) + k;
   return num / denom;
 }
 
-vec3 geometric(vec3 w_o, vec3 w_i) {
-  vec3 k = pow((roughness + 1.0), vec3(2.0)) / 8.0;
-  vec3 shadowing = GSchlick(w_i, k);
-  vec3 obstruction = GSchlick(w_i, k);
+float geometric(vec3 w_o, vec3 w_i) {
+  float k = pow((roughness + 1.0), 2.0) / 8.0;
+  float shadowing = GSchlick(w_i, k);
+  float obstruction = GSchlick(w_o, k);
   return shadowing * obstruction;
 }
 
 vec3 brdf_diffuse(vec3 albedo) {
-  return albedo / 3.141592654;
+  return albedo / PI;
 }
 
-vec3 brdf_specular(vec3 w_o, vec3 w_i) {
-  vec3 D = normalize(normal_distrib(w_o, w_i));
-  vec3 G = normalize(geometric(w_o, w_i));
-  vec3 num = D;
-  float denom = 4.0 * dot(w_o, vNormalWS) * dot(w_i, vNormalWS);
+float brdf_specular(vec3 w_o, vec3 w_i) {
+  float D = normal_distrib(w_o, w_i);
+  float G = geometric(w_o, w_i);
+  float num = D * G;
+  float denom = 4.0 * clamp(dot(w_o, vNormalWS), 0.0, 1.0) * clamp(dot(w_i, vNormalWS), 0.0, 1.0);
   return num / denom;
 }
 
@@ -92,15 +88,15 @@ void main()
     vec3 w_i = normalize(uLights[i].position - vPositionWS.xyz);
     vec3 w_o = normalize(w_i + vViewDirectionWS);
     // dielectrics: f0 = 0.4
-    vec3 f0 = vec3(0.04);
-    vec3 metallic = vec3(0.5);
+    float f0 = 0.04;
+    float metallic = 0.1;
 
-    vec3 kS = FresnelSchlick(f0, w_i, w_o);
-    vec3 kD = (1.0 - kS) * (1.0 - metallic);
+    float kS = FresnelSchlick(f0, w_i, w_o);
+    float kD = (1.0 - kS) * (1.0 - metallic);
     vec3 diffuseBRDFEval = kD * brdf_diffuse(albedo);
-    vec3 specularBRDFEval = kS * brdf_specular(w_o, w_i);
+    float specularBRDFEval = kS * brdf_specular(w_o, w_i);
 
-    irradiance += 0.2 + (diffuseBRDFEval + specularBRDFEval) * uLights[i].color * uLights[i].intensity * max(dot(vNormalWS, w_i), 0.0);
+    irradiance += (diffuseBRDFEval + specularBRDFEval) * uLights[i].color * uLights[i].intensity * clamp(dot(vNormalWS, w_i), 0.0, 1.0);
     /*
     vec3 ray = uLights[i].position - vPositionWS.xyz;
     vec3 color = clamp(dot(vNormalWS, ray), 0.0, 1.0) * uLights[i].color;
